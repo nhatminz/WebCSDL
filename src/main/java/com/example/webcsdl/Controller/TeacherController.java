e8ad0e893ae510d7fd0a187c798e12253f79a0e7
DANG NHAT <phandangnhat6a2005@gmail.com>
2024-11-26 02:14:49 +0700

    update TeacherController

--- src/main/java/com/example/webcsdl/Controller/TeacherController.java
+++ src/main/java/com/example/webcsdl/Controller/TeacherController.java
@@ -2,20 +2,26 @@ package com.example.webcsdl.Controller;
 
 import com.example.webcsdl.Entity.Student;
 import com.example.webcsdl.Entity.Teacher;
+import com.example.webcsdl.Service.DepartmentServiceImpl;
 import com.example.webcsdl.Service.TeacherServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class TeacherController {
+    @Autowired
+    private DepartmentServiceImpl departmentServiceImpl;
 
-   @Autowired
+    @Autowired
     private TeacherServiceImpl teacherServiceImpl;
+
     @GetMapping("/Teachers")
     public String showTeacherManagement(Model model) {
         model.addAttribute("teachers", teacherServiceImpl.getAllTeacher());
         model.addAttribute("teacher", new Teacher());
+        model.addAttribute("departments", departmentServiceImpl.getAllDepartment());
         return "Teachers";
     }
 }

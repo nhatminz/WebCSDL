f346cc4548fb2b6368f076dab609dfc6e35ef0c1
vanquyen252005 <quyenphamvan636@gmail.com>
2024-11-27 23:59:26 +0700

    chuc nang add Major

--- src/main/java/com/example/webcsdl/Controller/MajorController.java
+++ src/main/java/com/example/webcsdl/Controller/MajorController.java
@@ -1,6 +1,8 @@
 package com.example.webcsdl.Controller;
 
+import com.example.webcsdl.Entity.Department;
 import com.example.webcsdl.Entity.Major;
+import com.example.webcsdl.Entity.MajorDto;
 import com.example.webcsdl.Entity.StudentDto;
 import com.example.webcsdl.Service.DepartmentServiceImpl;
 import com.example.webcsdl.Service.MajorServiceImpl;
@@ -9,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MajorController {
@@ -18,14 +22,27 @@ public class MajorController {
     @Autowired
     private DepartmentServiceImpl departmentServiceImpl;
 
-    @Autowired
-   public MajorServices majorServices;
-
     @GetMapping("/Major")
     public String showMajorManagement(Model model) {
         model.addAttribute("majors", majorServiceImpl.getAllMajor());
-        model.addAttribute("major", new Major());
+        model.addAttribute("major", new MajorDto());
         model.addAttribute("departments", departmentServiceImpl.getAllDepartment());
         return "Major";
+    }
+    @PostMapping("/Majors/add")
+    public String addMajor(@ModelAttribute("major") MajorDto majorDto) {
+        majorServiceImpl.saveMajor(toEntity(majorDto));
+        return "redirect:/Major";
+    }
+
+    public Major toEntity(MajorDto majorDto) {
+        Major result = new Major();
+        result.setId(majorDto.getId());
+        result.setMajorName(majorDto.getMajorName());
+        result.setDescription(majorDto.getDescription());
+
+        Department department = departmentServiceImpl.getById(majorDto.getDepartmentId());
+        result.setDepartment(department);
+        return result;
     }
 }

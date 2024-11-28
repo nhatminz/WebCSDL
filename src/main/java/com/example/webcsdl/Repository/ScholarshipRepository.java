9681b244d1784f76100f7afdf93352ec9f18f2e5
DANG NHAT <phandangnhat6a2005@gmail.com>
2024-11-29 03:16:28 +0700

    function: search,delete,update scholarship

--- src/main/java/com/example/webcsdl/Repository/ScholarshipRepository.java
+++ src/main/java/com/example/webcsdl/Repository/ScholarshipRepository.java
@@ -3,6 +3,8 @@ package com.example.webcsdl.Repository;
 import com.example.webcsdl.Entity.Scholarship;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ScholarshipRepository extends JpaRepository<Scholarship, Long> {
+import java.util.List;
 
+public interface ScholarshipRepository extends JpaRepository<Scholarship, Long> {
+    List<Scholarship> findByScholarshipNameContainingIgnoreCase(String name);
 }

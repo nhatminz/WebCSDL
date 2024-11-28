53d57ccc82638848a7e857ebc9b45e9c55ebdd1a
pham nhat minh <phamnhatminh2k5@gmail.com>
2024-11-28 20:10:53 +0700

    function: update teacher

--- src/main/java/com/example/webcsdl/Controller/TeacherController.java
+++ src/main/java/com/example/webcsdl/Controller/TeacherController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -35,6 +36,25 @@ public class TeacherController {
         return "redirect:/Teachers";
     }
 
+    @GetMapping("/Teachers/update/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        Teacher teacher = teacherServiceImpl.getById(id);
+        if (teacher == null) {
+            throw new RuntimeException("teacher not found");
+        }
+        TeacherDto teacherDto = toDto(teacher);
+        model.addAttribute("teacher", teacher);
+        model.addAttribute("teacherDto", teacherDto);
+        model.addAttribute("departments", departmentServiceImpl.getAllDepartment());
+        return "updateTeacherForm";
+    }
+
+    @PostMapping("/Teachers/save")
+    public String updateTeacher(@ModelAttribute("teacher") TeacherDto teacherDto) {
+        teacherServiceImpl.saveTeacher(toEntity(teacherDto));
+        return "redirect:/Teachers";
+    }
+
     public Teacher toEntity(TeacherDto teacherDto) {
         Teacher teacher = new Teacher();
         teacher.setId(teacherDto.getId());
@@ -48,5 +68,15 @@ public class TeacherController {
         return teacher;
     }
 
+    public TeacherDto toDto(Teacher teacher) {
+        TeacherDto teacherDto = new TeacherDto();
+        teacherDto.setId(teacher.getId());
+        teacherDto.setFirstName(teacher.getFirstName());
+        teacherDto.setLastName(teacher.getLastName());
+        teacherDto.setEmail(teacher.getEmail());
+        teacherDto.setPhoneNumber(teacher.getPhoneNumber());
+        teacherDto.setDepartmentId(teacher.getDepartment().getId());
+        return teacherDto;
+    }
 
 }

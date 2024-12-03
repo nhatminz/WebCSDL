244fae0b461472f8113631c1b8547693f85bd929
pham nhat minh <phamnhatminh2k5@gmail.com>
2024-12-03 18:50:57 +0700

    update search major

--- src/main/java/com/example/webcsdl/Repository/MajorRepository.java
+++ src/main/java/com/example/webcsdl/Repository/MajorRepository.java
@@ -2,9 +2,16 @@ package com.example.webcsdl.Repository;
 
 import com.example.webcsdl.Entity.Major;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MajorRepository extends JpaRepository<Major, Long> {
-    List<Major> findByMajorNameContainingIgnoreCase(String majorName);
+
+    @Query("SELECT m FROM Major m WHERE " +
+            "LOWER(m.majorName) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(m.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(m.department.departmentName) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Major> searchMajors(@Param("keyword") String keyword);
 }

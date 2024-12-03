db61ba9f10f0ce40fa395a80bbdbd86b54ccec78
DANG NHAT <phandangnhat6a2005@gmail.com>
2024-12-03 15:08:43 +0700

    fix

--- src/main/java/com/example/webcsdl/Repository/StudentRepository.java
+++ src/main/java/com/example/webcsdl/Repository/StudentRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    List<Student> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(String firstName, String className);
+    List<Student> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(String firstName, String lastName);
 }
